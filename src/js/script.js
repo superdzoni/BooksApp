897a0fc11291186d8fc3ab87d793a72984e116d0
Joanna Olszewska <j.olszewskaa@gmail.com>
2022-08-24 11:35:16 +0200

    Fix initActions

--- src/js/script.js
+++ src/js/script.js
@@ -24,7 +24,7 @@
   let favoriteBooks = [];
 
   function render() {
-    for(let book of dataSource.books){
+    for(let book of dataSource.books) {
       const generatedHTML = templates.bookTemplate(book);
       const bookDOMElement = utils.createDOMFromHTML(generatedHTML);
       const bookListContainer = document.querySelector(select.listOf.bookList);
@@ -36,21 +36,21 @@
   initActions();
 
   function initActions() {
-    const imageContainer = document.querySelectorAll(select.containerOf.image);
-    for( const image of imageContainer){
-      image.addEventListener('dblclick', function(event){
-        event.preventDefault();
-        const bookId = event.target.getAttribute('data-id');
-        console.log(favoriteBooks);
-        if(!favoriteBooks.includes(bookId)){
+    const imageContainer = document.querySelector(select.listOf.bookList);
+    imageContainer.addEventListener('dblclick', function(event){
+      event.preventDefault();
+      if(event.target.offsetParent.classList.contains('book__image')) {
+        const bookId = event.target.offsetParent.getAttribute('data-id');
+        if(!favoriteBooks.includes(bookId)) {
           favoriteBooks.push(bookId);
           event.target.offsetParent.classList.add('favorite');
-        } else{
+        } else {
           const indexOfbookId = favoriteBooks.indexOf(bookId);
           favoriteBooks.splice(indexOfbookId, 1);
           event.target.offsetParent.classList.remove('favorite');
         }
-      });
-    }
+      }
+    });
+
   }
 }
